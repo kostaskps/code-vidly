9283f37c95d73afa2c0935078562b12c324be013
kostaskps <kapsalisk.develop@hotmail.com>
2021-04-20 17:13:18 +0300

    Add MembershipType

---
+++ VidlyApp/Vidly.Web/Models/MembershipType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Vidly.Web.Models
+{
+    public class MembershipType
+    {
+        public byte Id { get; set; }
+        public short SignupFee { get; set; }
+        public byte DurationInMonths { get; set; }
+        public byte DiscountRate { get; set; }
+    }
+}
