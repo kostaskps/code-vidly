75d9856f5261c52b39e3da3b8cce44fadce85b18
kostaskps <kapsalisk.develop@hotmail.com>
2021-04-20 17:07:05 +0300

    add customer IsSubscribedToNewsletter

---
+++ VidlyApp/Vidly.Web/Migrations/20210420140519_IsSubscribedToNewsletter.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Vidly.Web.Migrations
+{
+    public partial class IsSubscribedToNewsletter : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsSubscribedToNewsletter",
+                table: "Customers",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsSubscribedToNewsletter",
+                table: "Customers");
+        }
+    }
+}
