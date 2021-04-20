1abcbaa9affbe5b5b28104d0cd11c2c0f7a6f3db
kostaskps <kapsalisk.develop@hotmail.com>
2021-04-20 19:58:39 +0300

    Apply Annotations To CustomerName

--- VidlyApp/Vidly.Web/Migrations/20210420165726_ApplyAnnotationsToCustomerName.Designer.cs
+++ VidlyApp/Vidly.Web/Migrations/20210420165726_ApplyAnnotationsToCustomerName.Designer.cs
@@ -2,15 +2,17 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Vidly.Web.DataAccess;
 
 namespace Vidly.Web.Migrations
 {
     [DbContext(typeof(VidlyDBContext))]
-    partial class VidlyDBContextModelSnapshot : ModelSnapshot
+    [Migration("20210420165726_ApplyAnnotationsToCustomerName")]
+    partial class ApplyAnnotationsToCustomerName
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
