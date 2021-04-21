bce799895674022deb7687c4e034bc7bfc8b3a5a
kostaskps <kapsalisk.develop@hotmail.com>
2021-04-21 17:59:48 +0300

    Add movie details

--- VidlyApp/Vidly.Web/Controllers/MoviesController.cs
+++ VidlyApp/Vidly.Web/Controllers/MoviesController.cs
@@ -1,57 +1,41 @@
-﻿using System.Collections.Generic;
+﻿using System.Linq;
 using Microsoft.AspNetCore.Mvc;
-using Vidly.Web.Models;
-using Vidly.Web.ViewModels;
+using Microsoft.EntityFrameworkCore;
+using Vidly.Web.DataAccess;
 
 namespace Vidly.Web.Controllers
 {
     public class MoviesController : Controller
     {
+        private VidlyDBContext _dbContext;
+
+        public MoviesController(VidlyDBContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
         public IActionResult Index()
         {
-            var movies = ProvideMovies();
+            var movies = _dbContext.Movies.Include(c => c.Genre);
 
             return View(movies);
         }
-        public IActionResult Random()
-        {
-            var movie = new Models.Movie() { Name = "Shrek!" };
-            var customers = new List<Customer>
-            {
-                new Customer {Name = "Customer 1"},
-                new Customer {Name = "Customer 2"}
-            };
-            var viewModel = new RandomMovieViewModel
-            {
-                Movie = movie,
-                Customers = customers
-            };
 
-            return View(viewModel);
+        [Route("movies/details/{id?}")]
+        public IActionResult Details(int? Id)
+        {
+            if (!Id.HasValue)
+                Id = 0;
+
+            var movie = _dbContext.Movies.Include(c => c.Genre).SingleOrDefault(c => c.Id == Id);
+            if (movie == null)
+                return NotFound();
+            return View(movie);
         }
 
         public IActionResult Edit(int id)
         {
             return Content("id = " + id);
         }
-
-
-
-        [Route("movies/released/{year}/{month:regex(\\d{{2}}):range(1,12)}")]
-        public IActionResult ByReleaseDate(int year, int month)
-        {
-            return Content(year + "/" + month);
-        }
-
-        private IEnumerable<Movie> ProvideMovies()
-        {
-            var movies = new List<Movie>
-            {
-                new Movie { Id = 1, Name = "Shrek!"},
-                new Movie { Id = 2, Name = "Wall-e"}
-            };
-            return movies;
-        }
-
     }
 }
